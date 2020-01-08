cf2ed24ad4547bd08b7e17a0b377a7b406d2e65c
pieterhop <pieterhoppenbrouwers@hotmail.com>
2020-01-08 12:06:07 +0100

    Grid

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-class Grid:
-    def __init__(self, x, y, cap):
-        self.x = x
-        self.y = y
-        self.cap = capacity
-        self.test = test
-        self.ruft = ruft
-        self.tjak = tjak
+class Grid(object):
+    def __init__(self, x, y):
+        self.x = [0:50]
+        self.y = [0:50]
+        self.grid = [self.x, self.y]
 
-    def test(self):
-        print('hallo')
-        self.hallo = hallo
+    def populate(self):
+        for coord in self.grid:
+            print coord
