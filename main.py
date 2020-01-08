6e405f7db4fb2e72debf11b9470ebc6dc9ffc81f
pieterhop <pieterhoppenbrouwers@hotmail.com>
2020-01-08 13:39:23 +0100

    Main

--- main.py
+++ main.py
@@ -5,19 +5,17 @@ with open('input/wijk1_huizen.csv') as houses:
     for row in csv_reader:
         pass
 
-class Grid:
-    def __init__(self, x, y):
-        self.x = list(range(0, 50))
-        self.y = list(range(0, 50))
-        self.grid = [self.x, self.y]
-
-        def populate(self):
-            for coord in self.grid:
-                print coord
-
 class Battery:
     def __init__(self, x, y, cap):
         self.x = x
         self.y = y
         self.cap = capacity
         self.price = price
+
+def main():
+    x = list(range(0, 50))
+    y = list(range(0, 50))
+    grid = [x, y]
+
+    for coord in grid:
+            print coord
