f3c7eacdb9b1ad5972b2f0013d4d56633ee4e110
Reinier Boon <reinierboon@wcw-stud-145-109-39-234.wireless.uva.nl>
2020-01-08 12:11:47 +0100

    House class added

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import csv
+
 class Grid:
     def __init__(self, x, y, cap):
         self.x = x
@@ -9,3 +11,23 @@ class Grid:
     def test(self):
         print('hallo')
         self.hallo = hallo
+
+
+class House:
+    def __init__(self, x, y, out):
+        self.x = x
+        self.y = y
+        self.out = out
+
+house_list = []
+
+with open('wijk1_huizen.csv', newline='') as csv_file:
+    reader = csv.reader(csv_file)
+    next(reader, None)
+
+    for x, y, output in reader:
+        x = int(x)
+        y = int(y)
+        out = float(output)
+
+        house_list.append(House(x, y, out))
