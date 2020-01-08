f9bdce29f2806d6fbd11d033c7e234d7d7129c7f
pieterhop <pieterhoppenbrouwers@hotmail.com>
2020-01-08 14:11:48 +0100

    Merge

--- main.py
+++ main.py
@@ -32,15 +32,9 @@ class Houses:
         self.cables = cables
 
 def main():
-<<<<<<< HEAD
     print(import_file('input/wijk1_huizen.csv'))
     x = list(range(0, 51))
     y = list(range(0, 51))
-=======
-    import_file('input/wijk1_huizen.csv')
-    x = list(range(0, 50))
-    y = list(range(0, 50))
->>>>>>> 4a62709e28d8d2f61d059d6ebd3067b3c0b74f78
     grid = [x, y]
 
     for coord in grid:
