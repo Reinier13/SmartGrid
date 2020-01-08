a39fbdad0be075f48d7ae2c0f2c5345541a08b85
pieterhop <pieterhoppenbrouwers@hotmail.com>
2020-01-08 13:33:25 +0100

    Range

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-class Grid(object):
+import csv
+
+with open('input/wijk1_huizen.csv') as houses:
+    csv_reader = csv.reader(houses, delimiter=',')
+    for row in csv_reader:
+        pass
+
+class Grid:
     def __init__(self, x, y):
-        self.x = [0:50]
-        self.y = [0:50]
+        self.x = list(range(0, 50))
+        self.y = list(range(0, 50))
         self.grid = [self.x, self.y]
 
         def populate(self):
