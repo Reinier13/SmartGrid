339b3811fda5b5db131f9ebaaabfa2c3a615e6ab
pieterhop <pieterhoppenbrouwers@hotmail.com>
2020-01-08 14:18:04 +0100

    Clean

--- import_houses.py
+++ import_houses.py
@@ -1,7 +1,13 @@
+from main import House
 import csv
 
 def import_file(file):
-    with open(file) as houses_file:
-        houses = csv.reader(houses_file, delimiter=',')
-        for x, y, capacity in houses:
-            return x, y, capacity
+    with open(file, newline='') as houses_file:
+        houses = csv.reader(houses_file)
+        next(houses, None)
+        houses_list = []
+        for x, y, output in reader:
+            x = int(x)
+            y = int(y)
+            output = float(output)
+            house_list.append(House(x, y, output))
--- main.py
+++ main.py
@@ -1,32 +1,12 @@
 import csv
 
-class House:
-    def __init__(self, x, y, out):
-        self.x = x
-        self.y = y
-        self.out = out
-
-house_list = []
-
-with open('wijk1_huizen.csv', newline='') as csv_file:
-    reader = csv.reader(csv_file)
-    next(reader, None)
-
-    for x, y, output in reader:
-        x = int(x)
-        y = int(y)
-        out = float(output)
-
-        house_list.append(House(x, y, out))
-
-
 class Battery:
     def __init__(self, position, capacity):
         self.postition = postition
         self.capacity = capacity
         self.houses = houses
 
-class Houses:
+class House:
     def __init__(self, position, cables):
         self.position = position
         self.cables = cables
