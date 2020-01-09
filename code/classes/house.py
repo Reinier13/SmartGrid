b5e136564a721c034e78c2b9738b07a3e6059dbb
Reinier Boon <reinierboon@wcw-stud-145-109-2-208.wireless.uva.nl>
2020-01-09 16:10:26 +0100

    new try

--- code/classes/house.py
+++ code/classes/house.py
@@ -1,18 +1,11 @@
 class House(object):
     def __init__(self, x, y, output):
         self.x = x
-<<<<<<< HEAD
-        self.y = y
-        self.output = float(output)
-        self.cables = {}
-
-=======
         self.y = y.strip(' ')
         self.output = output.strip(' ')
 
     def add_cable(self):
         pass
->>>>>>> e1e2b4301f66039ef79b5b10621d17c3994a8a45
 
     def __repr__(self):
         return f"[{self.x}, {self.y}, {self.output}]"
