aece48e11e41deba3c466ff8537f479c7dd1919f
pieterhop <pieterhoppenbrouwers@hotmail.com>
2020-01-09 16:17:01 +0100

    Structure

---
+++ code/main.py
@@ -0,0 +1,9 @@
+from classes import grid
+from algorithms import random
+
+if __name__ == '__main__':
+    test_grid = grid.Grid('../input/wijk1_huizen.csv', '../input/wijk1_batterijen.csv')
+
+    random.random_connection(test_grid)
+
+    print(test_grid.batteries.houses)
