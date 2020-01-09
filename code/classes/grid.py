e1e2b4301f66039ef79b5b10621d17c3994a8a45
pieterhop <pieterhoppenbrouwers@hotmail.com>
2020-01-09 15:49:17 +0100

    Loading and plotting

--- code/classes/grid.py
+++ code/classes/grid.py
@@ -1,28 +1,47 @@
 from .house import House
 from .battery import Battery
 import csv
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 class Grid:
     def __init__(self, houses_file, batteries_file):
         self.houses = self.load_houses(houses_file)
-        self.batteries = self.load_batteries(batteries_file)
+        # self.batteries = self.load_batteries(batteries_file)
+        self.plot()
+
 
     def load_houses(self, houses_file):
         with open(houses_file, 'r') as houses_file:
-            houses = csv.DictReader(houses_file)
-            houses_list = []
-            for row in houses:
-                houses_list.append(House(row['x'], row[' y'], row[' max output']))
+            reader = csv.DictReader(houses_file)
+            houses = []
+            for row in reader:
+                houses.append(House(row['x'], row[' y'], row[' max output']))
+        print(houses)
+        houses_file.close()
+        return houses
 
-        return houses_list
+    # def load_batteries(self, batteries_file):
+    #     with open(batteries_file, 'r') as in_file:
+    #         batteries = csv.DictReader(in_file)
+    #         battery_dict = {}
+    #         for row in batteries:
+    #             battery_dict[row['positie']] = Battery(row['positie'], row[' capaciteit'])
+    #
+    #         print(battery_dict)
+    #
+    #     return battery_dict
 
-    def load_batteries(self, batteries_file):
-        with open(batteries_file, 'r') as in_file:
-            batteries = csv.DictReader(in_file)
-            battery_dict = {}
-            for row in batteries:
-                battery_dict[row['positie']] = Battery(row['positie'], row[' capaciteit'])
+    def plot(self):
+        fig, ax = plt.subplots()
+        for house in self.houses:
+            # print(house.x)
+            # print(house.y)
+            ax.plot(house.x, house.y)
 
-            print(battery_dict)
-
-        return battery_dict
+        ax.set(xlabel='X-axis', ylabel='Y-axis', title='Grid')
+        ax.grid()
+        fig.savefig("test.png")
+        plt.show()
--- code/classes/house.py
+++ code/classes/house.py
@@ -1,8 +1,11 @@
-class House:
+class House(object):
     def __init__(self, x, y, output):
         self.x = x
-        self.y = y
-        self.output = float(output)
+        self.y = y.strip(' ')
+        self.output = output.strip(' ')
+
+    def add_cable(self):
+        pass
 
     def __repr__(self):
-        return f"({self.x}, {self.y}) - output:{self.output}"
+        return f"[{self.x}, {self.y}, {self.output}]"
