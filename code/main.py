3472eda262ff70917e0856c269b96813524b374d
Reinier Boon <reinierboon@wcw-stud-145-109-2-208.wireless.uva.nl>
2020-01-10 13:36:44 +0100

    eerste deel random algortme met battery as list

--- code/main.py
+++ code/main.py
@@ -4,8 +4,10 @@ from algorithms import random
 if __name__ == '__main__':
     test_grid = grid.Grid('../input/wijk1_huizen.csv', '../input/wijk1_batterijen.csv')
 
-    # random.random_connection(test_grid)
+    random.random_connection(test_grid)
 
-    for battery in test_grid.batteries.values():
+    for battery in test_grid.batteries:
         print(battery.houses)
-        print(len(battery.houses))
+        break
+        # print(battery.houses)
+        # print(len(battery.houses))
