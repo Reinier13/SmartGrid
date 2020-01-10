4c1eee9bd7e2b4990c4cbdb235c693dde42564a0
Your Name <arminmirrezai@gmail.com>
2020-01-10 14:15:51 +0100

    aids

--- import_houses.py
+++ import_houses.py
@@ -6,8 +6,9 @@ def import_file(file):
         houses = csv.reader(houses_file)
         next(houses, None)
         houses_list = []
-        for x, y, output in reader:
+        for x, y, output in houses:
             x = int(x)
             y = int(y)
+            position = (x, y)
             output = float(output)
-            house_list.append(House(x, y, output))
+            houses_list.append(House(position, output))
