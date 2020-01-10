677e92e8131c412063f4a835b345e688b152148c
pieterhop <pieterhoppenbrouwers@hotmail.com>
2020-01-10 16:38:18 +0100

    hack

--- code/algorithms/greedy.py
+++ code/algorithms/greedy.py
@@ -3,6 +3,7 @@ import random
 def greedy(grid):
     num_houses = 0
     random.shuffle(grid.houses)
+    # shuffled = random.sample(grid.houses, len(grid.houses))
     for battery in grid.batteries:
         cap = battery.capacity
         for house in grid.houses:
@@ -11,6 +12,10 @@ def greedy(grid):
                 battery.add_house(house)
                 house.connected = True
         num_houses += len(battery.houses)
-        print(num_houses)
+    print(num_houses)
     if num_houses != len(grid.houses):
+        for house in grid.houses:
+            house.connected = False
         greedy(grid)
+
+    # K-HOLE PROBLEEM!!! ALS NIET IN 1 KEER GOED, DAN SHUFFLED LIJST ELKE KEER ZELFDE VOLGORDE
