4a5f1c60da0e8393a2ddca9d1e31008ec0e52b65
Reinier Boon <reinierboon@wcw-stud-145-109-33-230.wireless.uva.nl>
2020-01-13 15:39:50 +0100

    blegh

--- code/algorithms/algo.py
+++ code/algorithms/algo.py
@@ -1,28 +1,12 @@
-import random
-import numpy as np
+import random, math
 
 def greedy(grid):
     for house in grid.houses:
         for battery in grid.batteries:
             house.distances.append(distance(house, battery))
+        print(house.distances)
 
-        closest = house.distances.index(min(house.distances))
-        house.battery = grid.batteries[closest]
-
-        # counter = 0
-
-        if (house.battery.capacity_used() + house.output) < house.battery.capacity:
-            array = np.array(house.distances)
-            closest = house.distances.index(np.partition(array, 1)[1])
-
-            battery.add_house(house)
-            house.battery = grid.batteries[closest]
-            house.add_cable()
-
-            # counter += 1
-
-        print(house.battery)
-
+        
 
 def distance(house, battery):
     delta_x = house.x - battery.x
