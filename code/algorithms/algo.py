9acfc9942a1a33872e5b2994e761c88faca73d39
pieterhop <pieterhoppenbrouwers@hotmail.com>
2020-01-13 15:37:30 +0100

    h4ck

--- code/algorithms/algo.py
+++ code/algorithms/algo.py
@@ -1,14 +1,28 @@
-import random, math
+import random
+import numpy as np
 
 def greedy(grid):
     for house in grid.houses:
         for battery in grid.batteries:
             house.distances.append(distance(house, battery))
-        print(house.distances)
 
-    for house in house.distances:
-        if min(house.distances) != None:
-            print True
+        closest = house.distances.index(min(house.distances))
+        house.battery = grid.batteries[closest]
+
+        # counter = 0
+
+        if (house.battery.capacity_used() + house.output) < house.battery.capacity:
+            array = np.array(house.distances)
+            closest = house.distances.index(np.partition(array, 1)[1])
+
+            battery.add_house(house)
+            house.battery = grid.batteries[closest]
+            house.add_cable()
+
+            # counter += 1
+
+        print(house.battery)
+
 
 def distance(house, battery):
     delta_x = house.x - battery.x
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from code.classes import grid
 from code.algorithms import random, greedy, algo
+import numpy as np
 
 if __name__ == '__main__':
     test_grid = grid.Grid('input/wijk1_huizen.csv', 'input/wijk1_batterijen.csv')
 
-    greedy.greedy(test_grid)
+    algo.greedy(test_grid)
 
 
     test_grid.plot()
