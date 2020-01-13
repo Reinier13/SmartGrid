1f703ce21ed9a62c04d6b7b6904622c9193772a1
pieterhop <pieterhoppenbrouwers@hotmail.com>
2020-01-13 14:10:40 +0100

    calculate costs

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from code.algorithms import random, greedy
 
 if __name__ == '__main__':
     test_grid = grid.Grid('input/wijk1_huizen.csv', 'input/wijk1_batterijen.csv')
-
     greedy.greedy(test_grid)
-
     test_grid.plot()
+
+    print(test_grid.calculate_cost())
