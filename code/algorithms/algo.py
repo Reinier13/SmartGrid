782645a014597310505c447b8f177032ef57b2fe
pieterhop <pieterhoppenbrouwers@hotmail.com>
2020-01-13 14:51:30 +0100

    sg

--- code/algorithms/algo.py
+++ code/algorithms/algo.py
@@ -1,12 +1,17 @@
 import random, math
 
 def greedy(grid):
-    num_houses = 0
-    for battery in grid.batteries:
-        for house in grid.houses:
-            delta_x = house.x - battery.x
-            delta_y = house.y - battery.y
-            delta = delta_x + delta_y
+    for house in grid.houses:
+        for battery in grid.batteries:
+            house.distances.append(distance(house, battery))
+        print(house.distances)
 
-             for battery in battery.distances:
-                 min
+    for house in house.distances:
+        if min(house.distances) != None:
+            print True
+
+def distance(house, battery):
+    delta_x = house.x - battery.x
+    delta_y = house.y - battery.y
+    delta = abs(delta_x) + abs(delta_y)
+    return delta
