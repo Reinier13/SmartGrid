f812561fa74ec8ccc5ce467ed2ac5093b02fa7a3
Your Name <arminmirrezai@gmail.com>
2020-01-15 11:30:47 +0100

    clean up main.py

--- main.py
+++ main.py
@@ -5,11 +5,7 @@ import numpy as np
 if __name__ == '__main__':
     test_grid = grid.Grid('input/wijk1_huizen.csv', 'input/wijk1_batterijen.csv')
 
-<<<<<<< HEAD
-    master.greedy(test_grid)
-=======
     draft.draft(test_grid)
->>>>>>> 79964bd2da2dc16fce92d2b23a703d03365a65ad
 
     test_grid.plot()
 
