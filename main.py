ceb98b6a6ee5ca8dfd5bc269ef7c2301e9c7bd52
pieterhop <pieterhoppenbrouwers@hotmail.com>
2020-01-20 11:20:14 +0100

    Blub

--- main.py
+++ main.py
@@ -4,24 +4,9 @@ import numpy as np
 
 if __name__ == '__main__':
     test_grid = grid.Grid('input/wijk1_huizen.csv', 'input/wijk1_batterijen.csv')
-<<<<<<< HEAD
-    list = []
-    for i in range(10):
-        greedy.greedy(test_grid)
-        swap.hill_climb(test_grid)
-        list.append(test_grid.calculate_cost())
-    print(min(list))
-
-
-
-    # test_grid.plot()
-
-    print(test_grid.calculate_cost())
-=======
     costs = []
-    for i in range(30):
+    for i in range(50):
         greedy.greedy(test_grid)
         swap.hill_climb(test_grid)
         costs.append(test_grid.calculate_cost())
     test_grid.histogram(costs)
->>>>>>> f425cf1b3bca9049e8805e8a5b10a137575720c5
