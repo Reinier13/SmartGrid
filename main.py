df13854f38dea3f44f1ed69db606487419bc2ce1
pieterhop <pieterhoppenbrouwers@hotmail.com>
2020-01-24 13:55:43 +0100

    ruft

--- main.py
+++ main.py
@@ -11,35 +11,16 @@ if __name__ == '__main__':
 
     # generate multiple grids and apply hillclimb algorithm
     for i in range(iterations):
-        random.rand(test_grid)
-<<<<<<< HEAD
+        draft.draft(test_grid)
         # print(test_grid.calculate_cost())
         swap.hill_climb(test_grid)
         # multiple_swap.hill_climb(test_grid, 2)
-        # mst.mst(test_grid)
-=======
-
-        simanneal.simanneal(test_grid)
-        # swap.hill_climb(test_grid)
-<<<<<<< HEAD
-        # multiple_swap.hill_climb(test_grid)
-        print(test_grid.calculate_cost())
-        # mst.mst(test_grid)
-        # costs.append(test_grid.calculate_cost())
-
-=======
-        multiple_swap.hill_climb(test_grid, 1)
         mst.mst(test_grid)
->>>>>>> 4aff3dd5cf92e42bf2f4ace353981ffa332ebe93
         print(test_grid.calculate_cost())
->>>>>>> 571a31f58c631e31a737a5345cf44f8190bea8b6
 
     # print(test_grid.trees)
 
-<<<<<<< HEAD
-=======
     # display lowest cost
->>>>>>> 571a31f58c631e31a737a5345cf44f8190bea8b6
     # print(min(costs))
     # print(max(costs))
     # print(sum(costs)/len(costs))
