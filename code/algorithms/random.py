f527f09ce1406716aa92f1c0e380b537529108e0
Your Name <arminmirrezai@gmail.com>
2020-01-27 13:37:59 +0100

    random.py fix

--- code/algorithms/random.py
+++ code/algorithms/random.py
@@ -2,28 +2,28 @@ import random
 from code.classes import tree
 
 
-def rand(grid):
-    num_houses = 0
-    random.shuffle(grid.houses)
-    for battery in grid.batteries:
-        tree_obj = tree.Tree()
-        for house in grid.houses:
-            capacity_used = battery.capacity_used()
-            if battery.capacity > (capacity_used + house.output) and house.battery == None:
-                battery.add_house(house)
-                house.battery = battery
-                tree_obj.add_nodes(house, battery)
-        num_houses += len(battery.houses)
-        grid.trees.append(tree_obj.nodes)
+# def rand(grid):
+#     num_houses = 0
+#     random.shuffle(grid.houses)
+#     for battery in grid.batteries:
+#         tree_obj = tree.Tree()
+#         for house in grid.houses:
+#             capacity_used = battery.capacity_used()
+#             if battery.capacity > (capacity_used + house.output) and house.battery == None:
+#                 battery.add_house(house)
+#                 house.battery = battery
+#                 tree_obj.add_nodes(house, battery)
+#         num_houses += len(battery.houses)
+#         grid.trees.append(tree_obj.nodes)
 
-    if num_houses != len(grid.houses):
-        for battery in grid.batteries:
-            battery.houses = []
-            tree_obj.nodes = []
-        for house in grid.houses:
-            house.battery = None
-        grid.trees = []
-        rand(grid)
+#     if num_houses != len(grid.houses):
+#         for battery in grid.batteries:
+#             battery.houses = []
+#             tree_obj.nodes = []
+#         for house in grid.houses:
+#             house.battery = None
+#         grid.trees = []
+#         rand(grid)
 
 def rand(grid):
     num_houses = 0
@@ -35,8 +35,10 @@ def rand(grid):
                 battery.add_house(house)
                 house.battery = battery
         num_houses += len(battery.houses)
-    grid.draw()
 
     if num_houses != len(grid.houses):
         grid.clear()
         rand(grid)
+    else:
+        grid.draw()
+
