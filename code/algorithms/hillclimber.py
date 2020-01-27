eb7cffd96c53b7b733a1e03702959bb63704c857
Your Name <arminmirrezai@gmail.com>
2020-01-27 13:22:36 +0100

    pull

--- code/algorithms/hillclimber.py
+++ code/algorithms/hillclimber.py
@@ -1,47 +1,65 @@
 import random
+import copy
 
 class HillClimber:
     """
-    The HillClimber class that changes a random node in the graph to a random valid value. Each improvement or
+    The HillClimber class swaps random house(s) in a battery with another battery. Each improvement or
     equivalent solution is kept for the next iteration.
     """
-    def __init__(self, graph, transmitters):
-        if not graph.is_solution():
-            raise Exception("HillClimber requires a complete solution.")
+    def __init__(self, grid):
+        self.grid = copy.deepcopy(grid)
+        self.cost = grid.calculate_cost()
 
-        self.graph = copy.deepcopy(graph)
-        self.value = graph.calculate_value()
-
-        self.transmitters = transmitters
-
-    def mutate_single_node(self, new_graph):
+    def multiple_swap(self, grid, opt):
         """
-        Changes the value of a random node with a random valid value.
+        Swap multiple houses at once
         """
-        random_node = random.choice(list(new_graph.nodes.values()))
-        available_transmitters = random_node.get_possibilities(self.transmitters)
-        random_reconfigure_node(new_graph, random_node, available_transmitters)
+        swap_battery_1 = choose_battery(grid)
+        swap_houses_1 = []
+        for i in range(opt):
+            swap_house_1 = random.choice(swap_battery_1.houses)
+            while swap_house_1 in swap_houses_1:
+                swap_house_1 = random.choice(swap_battery_1.houses)
+            swap_houses_1.append(swap_house_1)
 
-    def mutate_graph(self, new_graph, number_of_nodes=1):
-        """
-        Changes the value of a number of nodes with a random valid value.
-        """
-        for _ in range(number_of_nodes):
-            self.mutate_single_node(new_graph)
+        swap_battery_2 = choose_battery(grid)
+        while swap_battery_1 == swap_battery_2:
+            swap_battery_2 = choose_battery(grid)
 
-    def check_solution(self, new_graph):
+        swap_houses_2 = []
+        for i in range(opt):
+            swap_house_2 = random.choice(swap_battery_2.houses)
+            while swap_house_2 in swap_houses_2:
+                swap_house_2 = random.choice(swap_battery_2.houses)
+            swap_houses_2.append(swap_house_2)
+
+        
+
+    def mutate_grid(self, swap_houses_1, swap_houses_2, swap_battery_1, swap_battery_2, opt):
+        """
+        Changes the battery-house configuration
+        """
+        for i in range(opt):
+            house_1 = swap_houses_1[i]
+            house_2 = swap_houses_2[i]
+
+            swap_battery_1.houses.append(swap_battery_2.houses.pop(swap_battery_2.houses.index(house_2)))
+            swap_battery_2.houses.append(swap_battery_1.houses.pop(swap_battery_1.houses.index(house_1)))
+        
+
+    def check_solution(self, new_grid):
         """
         Checks and accepts better solutions than the current solution.
         """
-        new_value = new_graph.calculate_value()
-        old_value = self.value
+        new_cost = new_grid.calculate_cost()
+        old_cost = self.cost
 
-        # We are looking for maps that cost less!
+        # We are looking for grids that cost less
         if new_value <= old_value:
-            self.graph = new_graph
-            self.value = new_value
+            self.grid = new_grid
+            self.cost = new_cost
 
-    def run(self, iterations, verbose=False, mutate_nodes_number=1):
+    def run(self, iterations, opt, verbose=False):
         """
         Runs the hillclimber algorithm for a specific amount of iterations.
         """
@@ -49,12 +67,12 @@ class HillClimber:
 
         for iteration in range(iterations):
             # Nice trick to only print if variable is set to True
-            print(f'Iteration {iteration}/{iterations}, current value: {self.value}') if verbose else None
+            print(f'Iteration {iteration}/{iterations}, current cost: {self.cost}') if verbose else None
 
             # Create a copy of the graph to simulate the change
-            new_graph = copy.deepcopy(self.graph)
+            new_grid = copy.deepcopy(self.grid)
 
-            self.mutate_graph(new_graph, number_of_nodes=mutate_nodes_number)
+            self.mutate_grid(new_grid, opt=opt)
 
             # Accept it if it is better
-            self.check_solution(new_graph)
+            self.check_solution(new_grid)
