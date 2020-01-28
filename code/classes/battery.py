d3301159405af5bf08e1b8e26fabe5e274793c35
Your Name <arminmirrezai@gmail.com>
2020-01-28 14:02:12 +0100

    battery class

--- code/classes/battery.py
+++ code/classes/battery.py
@@ -1,13 +1,19 @@
 class Battery:
-    def __init__(self, positie, capacity):
-        self.x = int(positie.split(', ')[0])
-        self.y = int(positie.split(', ')[-1])
+    """
+    Class Battery holds all necessary information about a battery, such as capacity and position.
+    """
+    def __init__(self, position, capacity):
+        self.x = int(position.split(', ')[0])
+        self.y = int(position.split(', ')[-1])
         self.capacity = float(capacity.strip(' '))
         self.cost = 5000
         self.houses = []
         self.distances = []
 
     def add_house(self, house):
+        """
+        Adds a house to the houses that are connected.
+        """
         self.houses.append(house)
 
     def capacity_used(self):
@@ -20,12 +26,21 @@ class Battery:
         return used_capacity
 
     def check_cap(self):
+        """
+        Checks whether the capacity of the battery is exceeded.
+        """
         if self.capacity_used() <= self.capacity:
             return True
 
     def clear(self):
+        """
+        Clears all the houses and "distances to houses" associated to the battery
+        """
         self.houses = []
         self.distances = []
 
     def __repr__(self):
+        """
+        Returns a readable overview when battery object is printed
+        """
         return f"[{self.x}, {self.y}, {self.capacity}]"
